7ad751272c8f18b467d64e6968ac436d8d61cf26
Rosalyn Bonilla <cookieroz@gmail.com>
2015-09-30 02:02:08 +0200

    Save angular-video-bg to bower

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-video-bg/angular-video-bg.min.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
