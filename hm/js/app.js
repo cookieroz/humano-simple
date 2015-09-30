10a693d6457b7c21c135d906cbee92de68a84cdb
Rosalyn Bonilla <cookieroz@gmail.com>
2015-09-30 12:56:25 +0200

    Add testing directory

---
+++ hm/js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name humanoApp
+ * @description
+ * # humanoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('humanoApp', [
+    'ngRoute',
+    'angularVideoBg'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'templates/main.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+angular.module('humanoApp').controller('VideoCtrl', function($scope) {
+  $scope.video = {
+    id: '2x113QS0Vls'
+  };
+  // create a mute and unmute button
+  // test on mobile
+});
